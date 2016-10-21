3e22558b4cd308fa35dd9f8987c6c5c35ab85687
Mark Hebert <mark@hasissues.com>
2016-10-20 20:40:25 -0400

    cleanup content

--- src/pages/read/read.ts
+++ src/pages/read/read.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Feeds } from '../../providers/feeds';
 
 @Component({
   selector: 'page-read',
@@ -7,9 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 
 export class ReadPage {
+  feed: any = [];
+  errorMessage: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public feeds: Feeds) {
+    this.loadData();
   }
 
+  loadData() {
+    this.feeds.load().then(
+      data => {
+        console.log(data.results.item);
+        this.feed = data.results.item;
+      },
+      error => { this.feed = []; this.errorMessage = <any>error; }
+    );
+
+  }
 }
--- src/providers/feeds.ts
+++ src/providers/feeds.ts
@@ -10,7 +10,8 @@ export class Feeds {
 
   load() {
     var feedUrl: string = "http://rss.nytimes.com/services/xml/rss/nyt/Business.xml";
-    var url: string = 'https://query.yahooapis.com/v1/public/yql?q=select%20title%2Clink%2Cdescription%20from%20rss%20where%20url%3D%22'+encodeURIComponent(feedUrl)+'%22&format=json';
+    //var url: string = 'https://query.yahooapis.com/v1/public/yql?q=select%20title%2Clink%2Cdescription%20from%20rss%20where%20url%3D%22'+encodeURIComponent(feedUrl)+'%22&format=json';
+    var url: string = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22'+encodeURIComponent(feedUrl)+'%22&format=json';
     if (this.data) {
       // already loaded data
       return Promise.resolve(this.data);
